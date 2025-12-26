51c05552c894cc01aee656aada1fb03c841fa3c6
ANUGRAHAA V <anugrahaa.venkatesh12@gmail.com>
2025-12-26 18:39:56 +0530

    Create profile.jsx

---
+++ multipage-dashboard/profile.jsx
@@ -0,0 +1,21 @@
+function Profile() {
+
+return (
+
+<div>
+
+<h2>Student Profile</h2>
+
+<p>Name: John Doe</p>
+
+<p>Roll No: 23BCS072</p>
+
+<p>Course: BCS</p>
+
+</div>
+
+);
+
+}
+
+export default Profile;
