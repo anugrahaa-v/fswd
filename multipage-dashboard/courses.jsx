73839f6695feb8d99a28c1eb6b96059a0cffe13c
ANUGRAHAA V <anugrahaa.venkatesh12@gmail.com>
2025-12-26 18:40:15 +0530

    Create courses.jsx

---
+++ multipage-dashboard/courses.jsx
@@ -0,0 +1,25 @@
+function Courses() {
+
+return (
+
+<div>
+
+<h2>My Courses</h2>
+
+<ul>
+
+<li>React JS</li>
+
+<li>Data Structures</li>
+
+<li>DBMS</li>
+
+</ul>
+
+</div>
+
+);
+
+}
+
+export default Courses;
